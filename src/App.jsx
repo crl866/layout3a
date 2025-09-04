7d993c8bbf92292288d3d3cfc21a0644e44a8800
carlanton <capdavid.student@ua.edu.ph>
2025-09-04 14:30:27 +0800

    footer replace name of the subject

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           <div className="rp">Related Posts</div>
         </div>
       </div>
-      <div className="footer">Footer</div>
+      <div className="footer">System Integration and Architecture Lecture</div>
     </div>
   )
 }
